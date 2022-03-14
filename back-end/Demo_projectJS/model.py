7e417d20fcefae645ccc3543e7440720a1b2f59b
duc minh <ngominhduc100@gmail.com>
2022-03-14 22:14:18 +0700

    add model address

--- back-end/Demo_projectJS/model.py
+++ back-end/Demo_projectJS/model.py
@@ -9,9 +9,11 @@ from flask_login import UserMixin
     
 
 class users:
-    def __init__(self, first_name = "", last_name = "", email = "", nation = "", Language = ""):
+    def __init__(self, first_name = "", last_name = "", email = "", nation = "", Language = "",address = "", age = 0):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.nation = nation
         self.Language = Language
+        self.age = age
+        self.address = address
