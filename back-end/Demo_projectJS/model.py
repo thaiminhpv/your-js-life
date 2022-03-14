3d8ce408b88d4eec316d708d7a829618a56c56ac
duc minh <ngominhduc100@gmail.com>
2022-03-14 20:59:52 +0700

    add model tempt

--- back-end/Demo_projectJS/model.py
+++ back-end/Demo_projectJS/model.py
@@ -1,24 +1,17 @@
 from datetime import datetime
 from email.policy import default
 import imp
+import string
 from time import timezone
 from django import db
 from sqlalchemy import Column, func
 from flask_login import UserMixin
+    
 
-
-class Data_User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(100))
-    last_name = db.Column(db.String(100))
-    email = db.Column(db.String(1000))
-    phone = db.Column(db.String(100))
-    address_user = db.Column(db.String(1000))
-    nation = db.Column(db.String(100))
-    slogan = db.Column(db.String(3000))
-    gender = db.Column(db.String(100))
-    dateofbirth_user = db.Column(db.DateTime, default=datetime.utcnow)
-    twitter = db.Column(db.String(100))
-    facebook = db.Column(db.String(100))
-    linked_in = db.Column(db.String(100))
-    google = db.Column(db.String(100))
+class users:
+    def __init__(self, first_name = "", last_name = "", email = "", nation = "", Language = ""):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.nation = nation
+        self.Language = Language
