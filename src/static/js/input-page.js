def3a15a36bec8a3cd571b28796331dbcded8c86
ninh <nguyentuanninh02@gmail.com>
2022-04-06 15:55:01 +0700

    update: upload photo,section my skills - v2

--- src/static/js/input-page.js
+++ src/static/js/input-page.js
@@ -1,2 +1,30 @@
-const message = 'Hello World';
-console.log(message);
+// upload avt
+const defaultBtn = document.querySelector("#default-btn");
+const customBtn = document.querySelector("#custom-btn");
+const img = document.querySelector("#avt");
+
+function defaultBtnActive() {
+    defaultBtn.click();
+};
+
+defaultBtn.addEventListener("change", function () {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function () {
+            const result = reader.result;
+            img.src = result;
+        }
+        reader.readAsDataURL(file);
+    }
+});
+//-------------------------------------------------
+
+// Input range My Skills
+const selectorValue = document.getElementById("selectorValue");
+const slider = document.getElementById("inputRange");
+
+selectorValue.innerHTML = slider.value + "%";
+slider.oninput = function () {
+    selectorValue.innerHTML = slider.value + "%";
+}
