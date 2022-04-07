5f48f7594265832ead68349b8e0430d41413fbc0
duc minh <ngominhduc100@gmail.com>
2022-04-07 13:37:32 +0700

    Merge file input-page.html branch 'feature/input-page' of https://github.com/thaiminhpv/your-js-life

--- app.py
+++ app.py
@@ -7,13 +7,12 @@ if os.path.isfile('./.env'):
     pass
 else:
     with open('./.env', mode='x') as f:
-        f.write("CLOUD_NAME = '" + input("CLOUD_NAME =")+ "'\n")
-        f.write("API_KEY = '" + input("API_KEY =") + '\n')
-        f.write("API_SECREAT = '" + input("API_SECREAT =") + "'\n")
-        f.write("DATABASE_HOST = '" + input("DATABASE_HOST =") + "'\n")
-        f.write("DATABASE_USER = '" + input("DATABASE_USER =") + "'\n")
-        f.write("DATABASE_PASSWORD = '" + input("DATABASE_PASSWORD =") + "'\n")
-        f.write("DATABASE_NAME = '" + input("DATABASE_NAME =") + "'\n")
+        f.write("CLOUD_NAME = '" + os.environ.get('CLOUD_NAME')+ "'\n")
+        f.write("API_KEY = '" + os.environ.get('API_KEY') + '\n')
+        f.write("API_SECREAT = '" + os.environ.get('API_SECREAT') + "'\n")
+        f.write("DATABASE_USER = '" + os.environ.get('DATABASE_USER') + "'\n")
+        f.write("DATABASE_PASSWORD = '" + os.environ.get('DATABASE_PASSWORD') + "'\n")
+        f.write("DATABASE_NAME = '" + os.environ.get('DATABASE_NAME') + "'\n")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
