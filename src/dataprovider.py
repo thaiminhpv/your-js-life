1953097015cc5570395519021febd01416fee348
duc minh <ngominhduc100@gmail.com>
2022-04-07 17:06:32 +0700

    add feature insert data to table services

--- src/dataprovider.py
+++ src/dataprovider.py
@@ -20,12 +20,23 @@ def get_id():
     return str(int(id)+1)
 
 
-def AddValueForTuple(database):
+def ConvertForTuple_Exp_Edu(database):
     list_result = list()
     dict_temp = dict()
     list_key = ["id", "portfolio_id", "title", "time", "content"]
     for item in database:
         tuple_temp = [(list_key[0],item[0]), (list_key[1],item[1]), (list_key[2],item[2]), (list_key[3],item[3]), (list_key[4],item[4])]
+        dict_temp = dict(tuple_temp)
+        list_result.append(dict_temp)
+    return list_result
+
+
+def ConvertForTuple_Services(database):
+    list_result = list()
+    dict_temp = dict()
+    list_key = ["id", "portfolio_id", "title", "description"]
+    for item in database:
+        tuple_temp = [(list_key[0],item[0]), (list_key[1],item[1]), (list_key[2],item[2]), (list_key[3],item[3])]
         dict_temp = dict(tuple_temp)
         list_result.append(dict_temp)
     return list_result
@@ -82,7 +93,7 @@ class InteractDatabase:
     def addportfolio(data_user):
         id = get_id() # id of new user
         parameter = model.Users.getuserlist(data_user)  # get data user with datatype: list
-        query = "INSERT INTO `your_js_life_database`.`portfolio` (`id`, `name`, `gmail`, `phone`, `address`, `dateofbirth`, `linkedin`, `facebook`, `github`, `job`, `workingtime`, `introduction`) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        query = "INSERT INTO `portfolio` (`id`, `name`, `gmail`, `phone`, `address`, `dateofbirth`, `linkedin`, `facebook`, `github`, `job`, `workingtime`, `introduction`) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
         InteractDatabase.executenonquery(query, parameter)
         return id
 
@@ -115,7 +126,7 @@ class InteractDatabase:
 
     def save_services(id, list_services):
         # insert service
-        query = "INSERT INTO `service` (`portfolio_id`, `title`, `description`) VALUES (%s, %s, %s)"
+        query = "INSERT INTO `services` (`portfolio_id`, `title`, `description`) VALUES (%s, %s, %s)"
         for row in list_services:
             parameter = (id, row['title'], row['description'])
             InteractDatabase.executenonquery(query, parameter)
@@ -134,13 +145,18 @@ class InteractDatabase:
         data = model.Users.getdatafromdb(result)
         return data
 
-    #
+
     def get_exp(id):
         database = InteractDatabase.executequery("SELECT * FROM `experience` WHERE `portfolio_id` = %s", (id,))
-        return AddValueForTuple(database)
+        return ConvertForTuple_Exp_Edu(database)
+
 
     def get_edu(id):
         database = InteractDatabase.executequery("SELECT * FROM `education` WHERE `portfolio_id` = %s", (id,))
-        return AddValueForTuple(database)
+        return ConvertForTuple_Exp_Edu(database)
 
+
+    def get_services(id):
+        database = InteractDatabase.executequery("SELECT * FROM `services` WHERE `portfolio_id` = %s", (id,))
+        return ConvertForTuple_Services(database)
 
