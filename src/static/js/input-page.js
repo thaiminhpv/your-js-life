cc28bf18ca2cfcdc2c35bd98f8fb582341fbc09f
Jasmine220 <93298442+Jasmine220@users.noreply.github.com>
2022-04-09 10:33:29 +0700

    .

--- src/static/js/input-page.js
+++ src/static/js/input-page.js
@@ -21,10 +21,42 @@ defaultBtn.addEventListener("change", function () {
 //-------------------------------------------------
 
 // Input range My Skills
-const selectorValue = document.getElementById("selectorValue");
-const slider = document.getElementById("inputRange");
+// const selectorValue = document.getElementById("selectorValue");
+// const slider = document.getElementById("inputRange");
 
-selectorValue.innerHTML = slider.value + "%";
-slider.oninput = function () {
-    selectorValue.innerHTML = slider.value + "%";
+// selectorValue.innerHTML = slider.value + "%";
+// slider.oninput = function () {
+//     selectorValue.innerHTML = slider.value + "%";
+// }
+
+// var mySkill_box = document.querySelector('.boxMySkills')
+// console.log(mySkill_box);
+// console.log('a');
+// var remove_fields = document.getElementById('remove_fields');
+
+// add_more_fields.onclick = function() {  
+//     var newField = mySkill_box.cloneNode(true);
+//     mySkill_box.append(newField);  
+// }
+// remove_fields.onclick = function() {
+//     var input_tags = document.getElementsByClassName('borderInputRange');
+//     console.log(input_tags);
+//     // if(input_tags.length > 1){
+//         mySkill_box.remove(input_tags[(input_tags.length) - 1 ]);
+//     // }
+// }
+function add() {
+        var newField = mySkill_box.cloneNode(true);
+        mySkill_box.append(newField); 
 }
+function remove() {
+        var input_tags = document.getElementsByClassName('borderInputRange');
+        console.log(input_tags);
+    // if(input_tags.length > 1){
+        mySkill_box.remove(input_tags[(input_tags.length) - 1 ]);
+}
+    
+
+
+
+
