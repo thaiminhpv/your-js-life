69f9e3948b51febad0885df294b8160bd0504d35
Jasmine220 <93298442+Jasmine220@users.noreply.github.com>
2022-04-09 23:25:26 +0700

    add name into input tag of MyEducation

--- src/static/js/input-page.js
+++ src/static/js/input-page.js
@@ -45,16 +45,48 @@ defaultBtn.addEventListener("change", function () {
 //         mySkill_box.remove(input_tags[(input_tags.length) - 1 ]);
 //     // }
 // }
-function add() {
-        var newField = mySkill_box.cloneNode(true);
-        mySkill_box.append(newField); 
+// function add() {
+//         var newField = mySkill_box.cloneNode(true);
+//         mySkill_box.append(newField); 
+// }
+// function remove() {
+//         var input_tags = document.getElementsByClassName('borderInputRange');
+//         console.log(input_tags);
+//     // if(input_tags.length > 1){
+//         mySkill_box.remove(input_tags[(input_tags.length) - 1 ]);
+// }
+
+function object(){
+    phone = document.querySelectorAll('input[name="phone"]')[0].value;
+    avatar = document.getElementById('avt').src;
+    job = document.querySelectorAll('input[name="job"]')[0].value;
+    //About Me
+    address = document.querySelectorAll('input[name="address"]')[0].value;
+    nickname = document.querySelectorAll('input[name="nickname"]')[0].value;
+    introduction = document.querySelectorAll('textarea[name="introduction"]')[0].value;
+    workingTime = document.querySelectorAll('input[name="workingtime"]')[0].value;
+    //Years of Experience
+    nameYearOfExperience = document.querySelectorAll('input[name="name"]')[0].value;
+    gmail = document.querySelectorAll('input[name="gmail"]')[0].value;
+    dateOfBirth = document.querySelectorAll('input[name="dateofbirth"]')[0].value;
+    addressYearOfExperience = document.getElementsByClassName('aboutMeInput')['address'].value;
+    facebook = document.getElementsByClassName('aboutMeInput')['facebook'].value;
+    github = document.getElementsByClassName('aboutMeInput')['github'].value;
+    linkedin = document.getElementsByClassName('aboutMeInput')['linkedin'].value;
+    //How I can help your next project
+    graphicDesign = document.querySelectorAll('input[placeholder="Graphic Design"]')[0].value;
+    textAreaGraphicDesign = document.querySelector('.text-muted.mb-0.form-control').value;
+    //MyEducation
+    experience = document.querySelectorAll('.borderEducation');
+    degreeMyEducation = document.querySelectorAll('.master')[0].value;
+    graduateUniversiry = document.querySelectorAll('.university')[0].value;
+    textareaMyEducation = document.getElementById('exampleFormControlTextarea1').value;
+    // document.querySelectorAll('.borderEducation')[0].querySelectorAll('textarea[id="exampleFormControlTextarea1"]')[0].value
+    
+    
+    
 }
-function remove() {
-        var input_tags = document.getElementsByClassName('borderInputRange');
-        console.log(input_tags);
-    // if(input_tags.length > 1){
-        mySkill_box.remove(input_tags[(input_tags.length) - 1 ]);
-}
+
     
 
 
